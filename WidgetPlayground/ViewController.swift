71c5d7091bbeb613a5ef3caf730bc579ef8f3f49
Hamid <hto+github@dixa.com>
2025-04-14 14:52:14 +0200

    Initial Commit

---
+++ WidgetPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetPlayground
+//
+//  Created by Hamidreza Tondkaranfar on 14/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
